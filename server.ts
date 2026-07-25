6c36195f1369c0071b8d05992626857521e3b1de
andripdotcom <sdm.umum.jabar3@gmail.com>
2026-07-25 15:24:18 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,3 +1,15 @@
+import mysql from "mysql2/promise";
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
+
 import express from "express";
 import path from "path";
 import multer from "multer";
